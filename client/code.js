ddf3e8d69a003b95344bd20bd5b2e6de93eec87c
Francis Irving <francis@flourish.org>
2015-10-19 15:01:18 +0100

    Really basic error handling

--- client/code.js
+++ client/code.js
@@ -6,15 +6,22 @@ $(function() {
         var url = $("#url").val()
         var certify_url = "https://jobschemacert.herokuapp.com/validate/?url=" + encodeURIComponent(url)
         $.get(certify_url, function(data) {
-            $('#go').removeAttr('disabled').text("Certify the job advert")
             console.log(data)
             if (data.valid) {
                 $("#passed").show()
                 $("#failed").hide()
+                $("#error").hide()
             } else {
                 $("#failed").show()
                 $("#passed").hide()
+                $("#error").hide()
             }
+        }).always(function() {
+            $('#go').removeAttr('disabled').text("Certify the job advert")
+        }).fail(function() {
+            $("#failed").hide()
+            $("#passed").hide()
+            $("#error").show()
         })
     })
 
