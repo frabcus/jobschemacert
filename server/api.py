64c79568e3c4f38c0b61ae3d1b7b70b5c429e14f
Sym Roe <sym.roe@talusdesign.co.uk>
2015-10-19 15:02:59 +0100

    Add static files

--- server/api.py
+++ server/api.py
@@ -1,9 +1,10 @@
 import json
+import os
 
 from flask.ext.cors import CORS
 from flask.ext.api import renderers
 from flask.ext.api import FlaskAPI
-from flask import request
+from flask import request, send_from_directory
 
 import requests
 from bs4 import BeautifulSoup
@@ -46,16 +47,20 @@ def validate_jobposting(url):
 
     return any(job_posting_found)
 
-@app.route('/example/')
-def example():
-    import ipdb; ipdb.set_trace()
-    return {'request data': request.get}
-
-
 @app.route('/validate/')
 def validate():
     url = request.values['url']
     return {'valid': validate_jobposting(url)}
+
+@app.route('/')
+def root():
+    return send_from_directory(os.path.abspath('client'), 'index.html')
+
+
+@app.route('/<path:path>')
+def home(path):
+    return send_from_directory(os.path.abspath('client/'), path)
+
 
 
 if __name__ == "__main__":
