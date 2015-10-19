911997becc8dbeff2a4de8fa604d18f06243c374
Francis Irving <francis@flourish.org>
2015-10-19 14:37:44 +0100

    Loading button disabled

--- client/code.js
+++ client/code.js
@@ -2,9 +2,11 @@
 
 $(function() {
     $("#go").click(function() {
+        $('#go').attr('disabled','disabled').text("Loading...")
         var url = $("#url").val()
         var certify_url = "https://jobschemacert.herokuapp.com/validate/?url=" + encodeURIComponent(url)
         $.get(certify_url, function(data) {
+            $('#go').removeAttr('disabled').text("Certify the job advert")
             console.log(data)
             if (data.valid) {
                 $("#passed").show()
