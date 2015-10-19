a0448b9ea88126d3ef18b36b49ebe9df2ae67422
Francis Irving <francis@flourish.org>
2015-10-19 14:08:08 +0100

    It's alive! Works and checks pages correctly.

--- client/code.js
+++ client/code.js
@@ -3,11 +3,10 @@
 $(function() {
     $("#go").click(function() {
         var url = $("#url").val()
-        var certify_url = "https://jobschemacert.herokuapp.com/?url=" + encodeURIComponent(url)
+        var certify_url = "https://jobschemacert.herokuapp.com/validate/?url=" + encodeURIComponent(url)
         $.get(certify_url, function(data) {
             console.log(data)
-            var obj = JSON.parse(data)
-            if (obj.valid) {
+            if (data.valid) {
                 $("#passed").show()
                 $("#failed").hide()
             } else {
