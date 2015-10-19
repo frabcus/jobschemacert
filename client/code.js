731ee663fe4420b4f6d4e76b83fe72ed7ea70fd7
Francis Irving <francis@flourish.org>
2015-10-19 13:33:24 +0100

    Basic querying

--- client/code.js
+++ client/code.js
@@ -1,8 +1,19 @@
+// jobschemacheck
+
 $(function() {
     $("#go").click(function() {
         var url = $("#url").val()
-        $.get(url, function(data) {
+        var certify_url = "https://jobschemacert.herokuapp.com/?url=" + encodeURIComponent(url)
+        $.get(certify_url, function(data) {
             console.log(data)
+            var obj = JSON.parse(data)
+            if (obj.valid) {
+                $("#passed").show()
+                $("#failed").hide()
+            } else {
+                $("#failed").show()
+                $("#passed").hide()
+            }
         })
     })
 
